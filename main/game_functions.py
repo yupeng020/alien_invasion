d1804520a585cdf5cc78188c29e43446f5da076a
yupeng020 <yupeng020@163.com>
2020-07-27 10:46:12 +0800

    在外星人舰队摧毁后，生成新的舰队

--- main/game_functions.py
+++ main/game_functions.py
@@ -74,7 +74,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(aliens, bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet position.
     bullets.update()
@@ -86,6 +86,12 @@ def update_bullets(aliens, bullets):
     # Check for any bullets that have hit aliens.
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy exiting bullets and create new fleet
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
 
 
 def get_number_aliens_x(ai_settings, alien_width):
--- main/settings.py
+++ main/settings.py
@@ -28,15 +28,15 @@ class Settings:
         self.ship_speed_factor = 16
 
         # Bullet settings
-        self.bullet_speed_factor = 5
-        self.bullet_width = 3
+        self.bullet_speed_factor = 12
+        self.bullet_width = 1200
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         # Limiting the number of buttlets
         self.bullets_allowed = 8
 
         # Alien settings
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 3
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
